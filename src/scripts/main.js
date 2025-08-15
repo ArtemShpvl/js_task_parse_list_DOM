738cc8283756421f58870237b02a2fc886499b0d
ArtemShpv <artem.shapoval99@gmail.com>
2025-08-16 01:10:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const employeesList = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
+
+function sortBySalary() {
+  const salarySorted = employeesList.sort(
+    (a, b) =>
+      parseFloat(b.dataset.salary.replace('$', '').replace(/,/g, '')) -
+      parseFloat(a.dataset.salary.replace('$', '').replace(/,/g, '')),
+  );
+
+  salarySorted.forEach((li) => ul.appendChild(li));
+}
+
+function getgetEmployees() {
+  return employeesList.map((el) => ({
+    name: el.textContent.trim(),
+    age: el.dataset.age,
+    position: el.dataset.position,
+    salary: parseFloat(el.dataset.salary.replace('$', '').replace(/,/g, '')),
+  }));
+}
+
+sortBySalary();
+
+const employeesObjArray = getgetEmployees();
+
+console.log(employeesObjArray);
